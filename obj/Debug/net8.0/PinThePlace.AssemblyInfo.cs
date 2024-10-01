5e25adaaf270ceae48e9973aeddabbff48267580
97illimo <114436711+97illimo@users.noreply.github.com>
2024-10-01 18:09:11 +0200

    Debug mode lagde en hau med nye filer
    Startet prosjektet i debug mode og det genererte mange nye filer.
    Disse må sikkert i gitignore?

--- obj/Debug/net8.0/PinThePlace.AssemblyInfo.cs
+++ obj/Debug/net8.0/PinThePlace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c62c415f15090e3ca5ac6dc06f0b462ac73803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05e06445d46cb37ad0b443ca969ba8464c9bbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
