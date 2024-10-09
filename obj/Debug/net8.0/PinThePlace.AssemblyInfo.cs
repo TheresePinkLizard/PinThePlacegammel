192ba469cfaea67fa9af59b61e1bd294b1395ad3
97illimo <114436711+97illimo@users.noreply.github.com>
2024-10-09 11:37:14 +0200

    assembly

--- obj/Debug/net8.0/PinThePlace.AssemblyInfo.cs
+++ obj/Debug/net8.0/PinThePlace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9262fd609d8b767c2f9086a09fc70109f92172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915ce263da676da324e680ab0a6a69b21b663c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PinThePlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
